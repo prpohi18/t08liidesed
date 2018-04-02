224c02fe4d42206840ebe6a388a12a957114975f
andremartov <31885522+andremartov@users.noreply.github.com>
2018-04-02 10:20:06 +0300

    Delete KolmnurgaYlesanneteTavalineLooja.java

--- src/main/java/kolmnurk/KolmnurgaYlesanneteTavalineLooja.java
+++
@@ -1,9 +0,0 @@
-package kolmnurk;
-public class KolmnurgaYlesanneteTavalineLooja implements KolmnurgaYlesanneteLoojaLiides{
-	@Override
-	public KolmnurgaYlesanne looKolmnurgaYlesanne(int maxpindala){
-		int a1=3;
-		int a2=2;
-		return new KolmnurgaYlesanne(a1, a2);
-	}
-}

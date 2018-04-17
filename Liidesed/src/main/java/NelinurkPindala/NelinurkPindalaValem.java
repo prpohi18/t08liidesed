5aff2326411e48ecd313dd8c579c3816473d8886
Egert Klaamas <31847333+egert112@users.noreply.github.com>
2018-04-17 13:38:28 +0300

    Kodutöö Tehtud NelinurkPindala

---
+++ Liidesed/src/main/java/NelinurkPindala/NelinurkPindalaValem.java
@@ -0,0 +1,8 @@
+package NelinurkPindala;
+public class NelinurkPindalaValem implements NelinurkPindalaLiides{
+    
+    public int leiaPindala(int külg, int külg1){
+        return külg*külg1;
+    }
+
+}

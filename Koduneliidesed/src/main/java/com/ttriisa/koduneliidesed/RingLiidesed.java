b5d95095f96ccb8fda77aa2328fef25b2700af31
zeus <zeus@A406-17.INFORMAATIKA>
2018-04-19 10:53:46 +0300

    Triisa kodune

---
+++ Koduneliidesed/src/main/java/com/ttriisa/koduneliidesed/RingLiidesed.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ttriisa.koduneliidesed;
+
+/**
+ *
+ * @author zeus
+ */
+public interface RingLiidesed {
+    public double umbermoot(double raadius);
+    public double pindala(double raadius);
+}

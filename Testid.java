eed99bc03db9d3dc6e52c0b3bf9afe688ba22910
Krislyn Luks <krsln@tlu.ee>
2018-05-30 18:15:25 +0300

    typo fix

--- Testid.java
+++ Testid.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class Testid {
 
   @Test
-  public void pyramiidKontroll(){
+  public void astendamiseKontroll(){
       Astendamine a1=new Astendamine(2, 3);
       Astendamine a2=new Astendamine(3, 2);
       assertEquals(8, a1.kysiAstendamine(), 0.1);
